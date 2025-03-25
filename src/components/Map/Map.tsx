66d91e4f7da1bed2306fb61435f9c59799b95c13
Rik <rik@HEZORG-FVFG63J4Q05N.local>
2025-03-25 18:39:38 +0100

    fix: Verbeterde OpenTopoMap configuratie

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -144,7 +144,9 @@ export default function Map() {
                   >
                     <TileLayer
                       url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
-                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                      attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a>'
+                      maxZoom={17}
+                      detectRetina={true}
                     />
                     {filteredLocations.map((location) => (
                       <Marker
